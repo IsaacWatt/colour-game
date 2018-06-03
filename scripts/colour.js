688b20662ce412b8b0aabb41f15d6cc58fdbe73e
Isaac Watt <isaacwatt@icloud.com>
2018-06-03 14:32:06 -0400

    Implements colour randomization

--- scripts/colour.js
+++ scripts/colour.js
@@ -1,16 +1,10 @@
-var colourList = [
-  "rgb(0, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 0, 0)",
-  "rgb(255, 0, 255)",
-  "rgb(255, 80, 0)",
-  "rgb(80, 0, 80)",
-];
+var colourList = makeColours(6); //array of 6 random colours
 
 var colourSquares = document.querySelectorAll(".colour-square");
-var correctColour = randomColour();
+var correctColour = colourList[Math.floor(Math.random() * colourList.length)];
 var displayedColour = document.getElementById("game-colour");
 var guessResult =  document.querySelector("#guess-result");
+var header = document.querySelector("h1");
 
 displayedColour.textContent = correctColour;
 
@@ -21,10 +15,11 @@ for (var square = 0; square < colourSquares.length; ++square) {
   // click listeners for squares
   colourSquares[square].addEventListener("click", function() {
     var colourClicked = this.style.backgroundColor;
-
+    console.log(colourClicked, correctColour);
     if (colourClicked === correctColour) {
       guessResult.textContent = "Correct Colour!";
       changeColours(colourClicked);
+      header.style.backgroundColor = correctColour;
     } else {
       this.style.backgroundColor = "rgb(50, 100, 120";
       guessResult.textContent = "Try Again!";
@@ -39,7 +34,21 @@ function changeColours(c) {
   }
 }
 
+function makeColours(numColours) {
+  var colours = [];
+
+  for (var i = 0; i < numColours; ++i) {
+    colours.push(randomColour());
+  }
+
+  return colours;
+}
+
 function randomColour() {
-  var randomIndex = Math.floor(Math.random() * colourList.length);
-  return colourList[randomIndex];
+  // pick an r,g,b from [0, 255]
+  var red = Math.floor(Math.random() * 256);
+  var green = Math.floor(Math.random() * 256);
+  var blue = Math.floor(Math.random() * 256);
+  var rgb = "rgb(" + red + ", " + green + ", " + blue +")";
+  return rgb;
 }
