5d60056f0551a1eb1b6d8f411786ab4222aeb391
Isaac Watt <isaacwatt@icloud.com>
2018-06-04 23:00:17 -0400

    implemets hard/easy mode

--- scripts/colour.js
+++ scripts/colour.js
@@ -1,4 +1,5 @@
-var colourList = makeColours(6); //array of 6 random colours
+var numSquares = 6;
+var colourList = makeColours(numSquares); //array of 6 random colours
 
 var colourSquares = document.querySelectorAll(".colour-square");
 var correctColour = colourList[Math.floor(Math.random() * colourList.length)];
@@ -7,8 +8,40 @@ var guessResult =  document.querySelector("#guess-result");
 var header = document.querySelector("h1");
 var restartBtn = document.querySelector("#restart");
 
+var easyBtn = document.querySelector("#easy");
+var hardBtn = document.querySelector("#hard");
+
+easyBtn.addEventListener("click", function() {
+  easyBtn.classList.add("selected");
+  hardBtn.classList.remove("selected");
+  numSquares = 3;
+  colourList = makeColours(numSquares);
+  correctColour = colourList[Math.floor(Math.random() * colourList.length)];
+
+  for (var square = 0; square < colourSquares.length; ++square) {
+    if ( colourList[square] ) {
+      colourSquares[square].style.backgroundColor = colourList[square];
+    } else {
+      colourSquares[square].style.display = "none";
+    }
+  }
+})
+
+hardBtn.addEventListener("click", function() {
+  easyBtn.classList.remove("selected");
+  hardBtn.classList.add("selected");
+  numSquares = 6;
+  colourList = makeColours(numSquares);
+  correctColour = colourList[Math.floor(Math.random() * colourList.length)];
+
+  for (var square = 0; square < colourSquares.length; ++square) {
+    colourSquares[square].style.backgroundColor = colourList[square];
+    colourSquares[square].style.display = "block";
+  }
+})
+
 restartBtn.addEventListener("click", function() {
-  colourList = makeColours(6);
+  colourList = makeColours(numSquares);
   var correctColour = colourList[Math.floor(Math.random() * colourList.length)];
   displayedColour.textContent = correctColour;
   for (var square = 0; square < colourSquares.length; ++square) {
